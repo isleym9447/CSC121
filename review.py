1f9d2f1fc23957b6a0e4e4c4e10db35dbeb0d7fb
isleym9447 <150169210+isleym9447@users.noreply.github.com>
2024-01-17 14:37:45 -0500

    Add files via upload

---
+++ review.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+
+# tell user what program does and ask for number
+
+print("program asks for num and multiplies it by 10\n")
+
+num =float(input("Enter a score: "))
+
+# evaluate if > 90
+
+if num > 90:
+    print("Student gets an A!")
+    print("\nWell Done! :) ")
+    
+else:
+    print("You failed :( ")
+
+#display result
+
+print("Bye!")
