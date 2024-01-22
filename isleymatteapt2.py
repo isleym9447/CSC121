bf04b05c0af267c5c65610a87624d630662ba459
isleym9447 <150169210+isleym9447@users.noreply.github.com>
2024-01-22 13:59:10 -0500

    Add files via upload

---
+++ isleymatteapt2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 22 13:56:33 2024
+
+@author: isleym9447
+"""
+
+
+#ask user for their income
+income = float(input("Enter your yearly income: "))
+years = float(input("Enters the number of years worked: "))
+
+# check
+
+if income >= 40_000 and years >= 2:
+    
+    
+    
+        print("Congrats! You are approved for the loan!")
+        
+else:
+    print("Sorry! You were not approved for the loan!")
