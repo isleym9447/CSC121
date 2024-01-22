68cfc33da71d7043ed526314a1194badb3b27569
isleym9447 <150169210+isleym9447@users.noreply.github.com>
2024-01-22 13:37:27 -0500

    nested structures

---
+++ IsleyMattea.py
@@ -0,0 +1,35 @@
+#Mattea Isley
+# 1-22-2024
+
+
+
+#ask user to enter score
+
+score = float(input("Enter your score!: "))
+          
+#evaluate the score
+
+if score >= 90:
+    
+    #tell user they get an A
+    print("Congrats! You got an A! :) ")
+else:
+    
+    if score >= 80:
+        
+        print("You got a B! Great Work!")
+    else:
+        
+            if score >= 70:
+                
+                print("You got a C. Keep working!")
+            else:
+                
+                    if score >= 60:
+                        
+                        print("You got a D. You can do better than that!")
+                    else:
+                        
+                        print("You got an F. :( ")
+
+
