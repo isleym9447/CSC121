866f88780bb4651b7e193fe2490de4a32137a442
isleym9447 <150169210+isleym9447@users.noreply.github.com>
2024-01-31 13:39:37 -0500

    Add files via upload

---
+++ voidMI.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+def main():
+    get_name()
+    
+    
+    
+def get_name():
+    
+    first = input("Enter your name: ")
+    
+    print ("Hello", first)
+    
+main()
