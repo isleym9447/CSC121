58d5ca5fb1a6ad5f4a5ca873d4222c580b2c03e1
isleym9447 <150169210+isleym9447@users.noreply.github.com>
2024-02-12 13:37:51 -0500

    Add files via upload

---
+++ unpacking.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+def testing():
+    one = int(input("Enter first num: "))
+    second = int(input("Enter second num: "))
+    
+    return one, second
+
+one, two = testing()
+
+total = one + two
+
+print(total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
